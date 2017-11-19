530752c615037b62c6ef8e6a7e9086a249855007
Krzysztof Rola <krzysztofrola@gmail.com>
2017-11-19 10:06:59 +0100

    Added new controlers

---
+++ Controllers/SessionController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ParentControlApi.Controllers
+{
+    [Authorize("Bearer")]
+    [Route("api/[controller]")]
+    public class SessionController : Controller
+    {
+        private UserRepository _userRepository;
+
+        public SessionController(){
+            _userRepository = new UserRepository();
+        }
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return _userRepository.GetAll();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
