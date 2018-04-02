b39c7a00a47c61c684b4c4ea5094350695ca5698
Krzysztof Rola <krzysztofrola@gmail.com>
2018-04-02 13:02:33 +0200

    Fixed session update

--- Mapper/MapperProfile.cs
+++ Mapper/MapperProfile.cs
@@ -13,7 +13,9 @@ public class MappingProfile : Profile {
         CreateMap<CreateScheduleParams, Schedule>();
         CreateMap<UpdateScheduleParams, Schedule>();
 
-        CreateMap<Session, SessionDTO>();
+        CreateMap<Session, SessionDTO>()
+            .ForMember(dest => dest.SessionStart, opt => opt.MapFrom(src => src.StarTime))
+            .ForMember(dest => dest.SessionEnd, opt => opt.MapFrom(src => src.EndTime));
         CreateMap<GetDateSessionsParams, Session>();
         CreateMap<CreateSessionParams, Session>()
             .ForMember(dest => dest.StarTime, opt => opt.MapFrom(src => src.SessionStart))
