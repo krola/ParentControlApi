66d0f8b308064b2a2176214072eed4e15960d719
Krzysztof Rola <krzysztofrola@gmail.com>
2018-11-27 22:11:04 +0100

    Fixed build problem

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Swashbuckle.AspNetCore.Swagger;
 using Websocket;
